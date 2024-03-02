d16bb7246cc4a2b8699637aef58ba490cce40d71
Nathan Huey <nate.huey4@gmail.com>
2024-03-02 10:22:16 -0800

    feat: keybinds for ToggleTerm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 -- lazy
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- toggleterm
+vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
+vim.keymap.set("t", "<C-x>", "<cmd>ToggleTermToggleAll<cr>")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
 require("lazy").setup({
   spec = {
     { import = "plugins" },
