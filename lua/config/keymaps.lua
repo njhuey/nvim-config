63f2bad380cf2ae4344776701fa09d6a5a5bca79
Nathan Huey <nate.huey4@gmail.com>
2024-03-02 18:04:52 -0800

    fix: change keymap for hide terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,4 @@ vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- toggleterm
 vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
-vim.keymap.set("t", "<C-x>", "<cmd>ToggleTermToggleAll<cr>")
+vim.keymap.set("t", "<C-h>", "<cmd>ToggleTermToggleAll<cr>")
