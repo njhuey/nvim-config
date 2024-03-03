bf3f762663b1395f1fd61213f2981ebaf2aa95eb
Nathan Huey <nate.huey4@gmail.com>
2024-03-02 23:06:01 -0800

    feat: add indent-blankline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -103,5 +103,32 @@ return {
         extensions = { "neo-tree", "lazy" },
       }
     end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
   }
 }
