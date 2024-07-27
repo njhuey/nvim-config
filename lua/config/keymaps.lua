b58c71645b979bbf3f809ab609bc0e0e1a8ef5b1
Nathan Huey <nate.huey4@gmail.com>
2024-07-27 00:06:57 -0400

    feat: add autocommand

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>ltl", ":set paste<CR>A  # NOQA: E501<ESC>:set nopaste<CR>")
+vim.keymap.set("n", "<leader>ti", ":set paste<CR>A  # type: ignore[]<ESC>:set nopaste<CR>")
